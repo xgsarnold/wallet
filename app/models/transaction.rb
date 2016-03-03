5268ca7453cde9860dc366a98ba3de277ff33122
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-03-02 23:19:19 -0500

    Pass test return balance.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,29 @@
 class Transaction < ActiveRecord::Base
+  def self.debits
+    self.all.select {|t| t.activity == "Debit"}
+  end
+
+  def self.credits
+    self.all.select {|t| t.activity == "Credit"}
+  end
+
+  def self.total_spent
+    debits.reduce(0) {|sum, debit| sum + debit.amount}
+  end
+
+  def self.total_income
+    credits.reduce(0) {|sum, credit| sum + credit.amount}
+  end
+
+  def self.total_balance
+    balance = total_income - total_spent
+    if balance < 0.00
+      "Hope you enjoy being a debt slave."
+    else
+      balance
+    end
+  end
+
   def self.total_transactions_now
     self.count
   end
